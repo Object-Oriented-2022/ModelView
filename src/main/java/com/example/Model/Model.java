085a6e2a64b1e51b3d571b100f73aeaff970d9b8
genejackett <genejackett@gmail.com>
2022-11-02 12:34:09 -0500

    Observable list names added to model.

--- src/main/java/com/example/Model/Model.java
+++ src/main/java/com/example/Model/Model.java
@@ -1,6 +1,8 @@
 package com.example.Model;
 
 import com.example.Teams.Teams;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -10,13 +12,6 @@ public class Model {
     private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     private static String timeStamp = dateFormat.format(new Date());
 
-    public static HashMap<Integer, Teams> teams = new HashMap<>(){{
-       put(1,new Teams("Lyons","23", timeStamp));
-        put(2,new Teams("Eagles","32", timeStamp));
-        put(3,new Teams("Bears","41", timeStamp));
-        put(4,new Teams("Dogs","50", timeStamp));
-        put(5,new Teams("Cats","68", timeStamp));
-    }};
-
-
+    ObservableList<String> names = FXCollections.observableArrayList(
+            "Julia", "Ian", "Sue", "Matthew", "Hannah", "Stephan", "Denise");
 }
