272b864b35f76daf59eff4cd36ed567879bdd398
crawlive <crawfolivia@gmail.com>
2022-11-02 12:33:03 -0500

    view and viewmodel files

---
+++ src/main/java/com/example/ViewModel/ViewModel.java
@@ -0,0 +1,10 @@
+package com.example.ViewModel;
+
+import com.example.Model.Model;
+import com.example.Teams.Teams;
+import javafx.scene.control.ListView;
+
+public class ViewModel {
+    ListView<String> list = new ListView<String>();
+
+}
